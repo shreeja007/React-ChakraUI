d5c579b09c4f5cc84d64fddf0d117a05c4a64a84
Shreeja Manandhar <manandharja@gmail.com>
2023-10-25 11:00:22 +0545

    home wipv1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 const Home = () => {
   return <Box>
